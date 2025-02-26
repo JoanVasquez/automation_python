de4f54b17199bf6a87f17431a26dd0fd8e9e7bc6
Joan Vasquez <dev.joanvasquez@gmail.com>
2025-02-26 16:45:26 +0000

    adding currency srapping and online retail login

---
+++ automation_selenium/online_retailer.py
@@ -0,0 +1,24 @@
+from .configurations import get_driver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+def main():
+  driver = get_driver(
+      "https://titan22.com/account/login?return_url=%2Faccount")
+  driver.find_element(by="id",
+                      value="CustomerEmail").send_keys("app7flask@gmail.com")
+  time.sleep(2)
+  driver.find_element(by="id",
+                      value="CustomerPassword").send_keys("??!65EhGMg8.WwY" +
+                                                          Keys.RETURN)
+  time.sleep(2)
+  driver.find_element(
+      by="xpath",
+      value=
+      '//*[@id="shopify-section-footer"]/section/div/div[1]/div[1]/div[1]/nav/ul/li[1]/a'
+  ).click()
+  print(driver.current_url)
+
+
+print(main())
--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-import automation_selenium.initial_selenium
-import automation_selenium.login_automation
+# import automation_selenium.initial_selenium
+# import automation_selenium.login_automation
+# import automation_selenium.online_retailer
+import automation_selenium.currency
